89100ee7f35f80e75fa13388222243c74f27dc3a
404-d <2933578709@qq.com>
2025-03-31 13:45:50 +0800

    modified:   .vscode/settings.json 	renamed:    sc.py -> VL53L0X.py 	modified:   motor.py

---
+++ motor.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO #type: ignore
+import time
+
+# 5v引脚为2号物理引脚
+IN1=17, IN2=18, speed=50
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+in1=GPIO.PWM(IN1, 50)
+in2=GPIO.PWM(IN2, 50)
+in1.start(0)
+in2.start(0)
+
+# IN1高电平，IN2低电平，电机正转
+# IN1低电平，IN2高电平，电机反转
+# 定义向左、向右、停止函数
+def left(ts):
+    in1.start(speed)
+    in2.start(0)
+    time.sleep(ts)
+
+def right(ts):
+    in1.start(speed)
+    in2.start(0)
+    time.sleep(ts)
+
+def stop():
+    in1.start(0)
+    in2.start(0)
