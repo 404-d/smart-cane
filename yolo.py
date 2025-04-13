eafb9b8a348d025ba38a542cfe8e761799005519
404-d <2933578709@qq.com>
2025-04-13 12:15:05 +0800

    new file:   yolo-old.py 	modified:   yolo.py 	deleted:    "\345\237\272\344\272\216\344\272\272\345\267\245\346\231\272\350\203\275\347\232\204\346\231\272\350\203\275\345\207\272\350\241\214\350\276\205\345\212\251\350\243\205\347\275\256\351\241\271\347\233\256\347\240\224\347\251\266\346\212\245\345\221\212.docx" 	deleted:    "\345\237\272\344\272\216\344\272\272\345\267\245\346\231\272\350\203\275\347\232\204\346\231\272\350\203\275\345\207\272\350\241\214\350\276\205\345\212\251\350\243\205\347\275\256\351\241\271\347\233\256\347\240\224\347\251\266\346\212\245\345\221\212.md" 	new file:   "\345\244\226\347\275\256\347\224\265\346\272\220\346\212\200\346\234\257\350\247\204\346\240\274.pdf" 	deleted:    "\345\276\205\345\212\236.md" 	deleted:    "\347\224\265\346\272\220.md"

--- yolo.py
+++ yolo.py
@@ -1,8 +1,10 @@
-import cv2 #type: ignore
-from picamera2 import Picamera2 #type: ignore
 from ultralytics import YOLO #type: ignore
+from picamera2 import Picamera2 #type: ignore
+import cv2 #type: ignore
+import sys #type: ignore
 
-# 初始化树莓派摄像头
+model = YOLO("yolo-v5.pt")
+
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (3280, 2464)
 picam2.preview_configuration.main.format = "RGB888"
@@ -10,39 +12,25 @@ picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
 
-# 加载YOLOv5模型
-model = YOLO("yolov5-16.pt")
+# 运行目标检测
+image = picam2.capture_array()
+results = model(image)
 
-while True:
-    # 截取一帧图像
-    frame = picam2.capture_array()
-    
-    # 识别并处存数据
-    results = model(frame)
-    
-    # Output the visual detection data, we will draw this on our camera preview window
-    annotated_frame = results[0].plot()
-    
-    # Get inference time
-    inference_time = results[0].speed['inference']
-    fps = 1000 / inference_time  # Convert to milliseconds
-    text = f'FPS: {fps:.1f}'
+# 提取检测结果并绘制矩形框
+for result in results:
+    boxes = result.boxes # 获取边界框信息
+    for box in boxes:
+        # 坐标
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        # 标签和准确度
+        cls_id = int(box.cls[0])
+        conf = float(box.conf[0])
+        cls_name = model.names[cls_id]
 
-    # 定义标签、位置
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text_size = cv2.getTextSize(text, font, 1, 2)[0]
-    text_x = annotated_frame.shape[1] - text_size[0] - 10  # 10 pixels from the right
-    text_y = text_size[1] + 10  # 10 pixels from the top
 
-    # Draw the text on the annotated frame
-    cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-    # Display the resulting frame
-    cv2.imshow("Camera", annotated_frame)
-
-    # Exit the program if q is pressed
-    # if cv2.waitKey(1) == ord("q"):
-    #     break
-    
-# Close all windows
-# cv2.destroyAllWindows()
+# # 矩形框
+# cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+# # 添加标签和准确度
+# label = f"{cls_name} {conf:.2f}"
+# cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
